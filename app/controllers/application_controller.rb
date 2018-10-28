85eacb61192fb06fe0d6830fe3f75a13c0c60000
Michael Roose <mjroose@gmail.com>
2018-10-28 22:40:30 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ class ApplicationController < Sinatra::Base
 
     if user.save
       session[:user_id] = user.id
-      redirect '/account'
+      redirect '/login'
     else
       redirect '/failure'
     end
